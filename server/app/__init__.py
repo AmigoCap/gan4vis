b5a710e1940d46431eaa074e60b5196bede44a89
Carlito <c.dupuy.mpsi@gmail.com>
2019-01-15 21:08:19 +0100

    First commit of server

---
+++ server/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
+from app import routes, models
